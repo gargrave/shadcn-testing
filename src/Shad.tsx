e09017bfb9276d8cfc43f725c6a2d3f3635dd322
Gabe Hargrave <gabe@Gabes-MacBook-Pro.local>
2023-08-08 12:57:18 -0500

    dialog, popover, etc

--- src/Shad.tsx
+++ src/Shad.tsx
@@ -1,19 +1,104 @@
+import React from "react";
 import { Heading } from "./components/typography";
-import { Button } from "./components/ui/";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "./components/ui/";
+
+const wait = () => new Promise((resolve) => setTimeout(resolve, 1000));
 
 function Shad() {
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
   return (
     <div>
       <Heading tag="h1">ShadCN</Heading>
 
       <Heading tag="h2">Button</Heading>
-      <div className="flex place-content-center gap-1">
+      <div className="flex flex-wrap place-content-center gap-1">
         <Button variant={"default"}>Primary</Button>
         <Button variant={"secondary"}>Secondary</Button>
         <Button variant={"destructive"}>Destructive</Button>
         <Button variant={"ghost"}>Ghost</Button>
         <Button variant={"link"}>Link</Button>
         <Button variant={"outline"}>Outline</Button>
+      </div>
+
+      <Heading tag="h2">Popover</Heading>
+      <div className="flex place-content-center">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline">Open popover</Button>
+          </PopoverTrigger>
+
+          <PopoverContent className="w-80">
+            <div className="grid gap-4">
+              <div className="space-y-2">
+                <h4 className="font-medium leading-none">Dimensions</h4>
+                <p className="text-sm text-muted-foreground">
+                  Set the dimensions for the layer.
+                </p>
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+
+      <Heading tag="h2">Dialog</Heading>
+      <div className="flex place-content-center">
+        <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+          <DialogTrigger asChild>
+            <Button variant="outline">Gimme a Dialog</Button>
+          </DialogTrigger>
+
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            {/* <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div> */}
+            <DialogFooter>
+              <Button
+                isLoading={isLoading}
+                onClick={() => {
+                  setIsLoading(true);
+                  wait().then(() => {
+                    setDialogOpen(false);
+                    setIsLoading(false);
+                  });
+                }}
+                type="submit"
+              >
+                Save changes
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,1 +1,3 @@
-export * from './button'
+export * from "./button";
+export * from "./dialog";
+export * from "./popover";
